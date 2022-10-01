f21d4f8d6375a42b3fc469cc536a8f3dff76318d
jklom0326 <jklom0326@gmail.com>
2022-10-01 22:42:17 +0900

    feat. recyclerView

--- app/src/main/java/com/example/partymaker/MainActivity.kt
+++ app/src/main/java/com/example/partymaker/MainActivity.kt
@@ -2,17 +2,26 @@ package com.example.partymaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.example.partymaker.databinding.ActivityMainBinding
+import com.jakewharton.threetenabp.AndroidThreeTen
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setMonthView()
     }
 
+    private fun setMonthView() {
+        AndroidThreeTen.init(this)
+        val selectedDate = LocalDateTime.now()
+        val formatted = DateTimeFormatter.ofPattern("yyyy년 MM월")
+        binding.monthYearTextView.text = selectedDate.format(formatted).toString()
+
+    }
 }
